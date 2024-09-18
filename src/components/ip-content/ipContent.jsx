202a6275ce1e17771a70eedfa5f5b51e72e50596
Ameya Taneja <ameyataneja1302@gmail.com>
2024-09-18 10:37:25 +0530

    card component created and who is api

--- src/components/ip-content/ipContent.jsx
+++ src/components/ip-content/ipContent.jsx
@@ -1,92 +1,100 @@
-import React from 'react'
-import './ipContent.css';
-import CustomCursor from "../../CustomCursor";
+import React, { useRef, useState } from "react";
+import "./ipContent.css";
+import WhoIsApi from 'whois-api-js';
+import fs from 'fs';
+import {config} from 'dotenv'
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
 
+import CardComponent from "../card-content/cardContent";
+const IptrackerContent = () => {
+  const [whoisData, setWhoisData] = useState(null);
+  const inputRef = useRef(null);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      console.log(inputValue);
+      const isValidUrl = validateUrl(inputValue);
+      if (isValidUrl) {
+        console.log("[VALIDATED Url: ]", inputValue);
+        setError("");
+      } else {
+        setError("Please Enter a Valid Url");
+      }
+    }
+  };
 
-const AboutContent = () => {
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+    setError("");
+  };
+
+  const validateUrl = (urlString) => {
+    const urlPattern =
+      /^(https?:\/\/)?([\da-z.-]+)\.([a-z]{2,6})(\/[\/\w \.-]*)*\/?$/;
+    return urlPattern.test(urlString);
+  };
+
+  const fetchData = async (url) => {
+    try {
+      const apiKey = process.env.whoISAPI;
+      if (!apiKey) {
+        console.error("Error: Missing API key from environment variables.");
+        return;
+      }
+
+      const client = new WhoisApi.Client(apiKey);
+      const whoisData = await client.get(url);
+      setWhoisData(whoisData);
+    } catch (error) {
+      console.error(`Error fetching data:`, error);
+      setError("Error fetching data. Please try again later.");
+    }
+  };
+
   return (
     <>
-      <CustomCursor></CustomCursor>
-      <div id ="pg1">
-        <div className="card-container">
-        <div className="card1">
-          <div className="card-body">
-          <h2>WHOISDOMAIN</h2>
-          <div class="css-15x27kp"><span class="lbl">Subject</span><span class="val" title="www.netflix.com">www.netflix.com</span></div>
-          <div class="css-15x27kp"><span class="lbl">Issuer</span><span class="val" title="DigiCert Inc">DigiCert Inc</span></div>
-          <div class="css-15x27kp"><span class="lbl">ASN1 Curve</span><span class="val" title="prime256v1">prime256v1</span></div>
-          <div class="css-15x27kp"><span class="lbl">NIST Curve</span><span class="val" title="P-256">P-256</span></div>
-          <div class="css-15x27kp"><span class="lbl">Expires</span><span class="val" title="25 October 2024">25 October 2024</span></div>
-          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
-          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
-          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
-          <div class="css-15x27kp"><span class="lbl">Subject</span><span class="val" title="www.netflix.com">www.netflix.com</span></div>
-          <div class="css-15x27kp"><span class="lbl">Issuer</span><span class="val" title="DigiCert Inc">DigiCert Inc</span></div>
-          <div class="css-15x27kp"><span class="lbl">ASN1 Curve</span><span class="val" title="prime256v1">prime256v1</span></div>
-          <div class="css-15x27kp"><span class="lbl">NIST Curve</span><span class="val" title="P-256">P-256</span></div>
-          <div class="css-15x27kp"><span class="lbl">Expires</span><span class="val" title="25 October 2024">25 October 2024</span></div>
-          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
-          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
-          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
-                    </div>
-          </div>
-        
-          <div className="card1">
-          <div className="card-body">
-          <h2>WHOISDOMAIN</h2>
-          <div class="css-15x27kp"><span class="lbl">Subject</span><span class="val" title="www.netflix.com">www.netflix.com</span></div>
-          <div class="css-15x27kp"><span class="lbl">Issuer</span><span class="val" title="DigiCert Inc">DigiCert Inc</span></div>
-          <div class="css-15x27kp"><span class="lbl">ASN1 Curve</span><span class="val" title="prime256v1">prime256v1</span></div>
-          
-          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
-          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
-          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
-          <div class="css-15x27kp"><span class="lbl">NIST Curve</span><span class="val" title="P-256">P-256</span></div>
-          <div class="css-15x27kp"><span class="lbl">Expires</span><span class="val" title="25 October 2024">25 October 2024</span></div>
-          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
-          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
-          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
-                    </div>
-          </div>
-          <div className="card1">
-          <div className="card-body">
-          <h2>WHOISDOMAIN</h2>
-          <div class="css-15x27kp"><span class="lbl">Subject</span><span class="val" title="www.netflix.com">www.netflix.com</span></div>
-          <div class="css-15x27kp"><span class="lbl">Issuer</span><span class="val" title="DigiCert Inc">DigiCert Inc</span></div>
-          <div class="css-15x27kp"><span class="lbl">ASN1 Curve</span><span class="val" title="prime256v1">prime256v1</span></div>
-          <div class="css-15x27kp"><span class="lbl">NIST Curve</span><span class="val" title="P-256">P-256</span></div>
-          <div class="css-15x27kp"><span class="lbl">Expires</span><span class="val" title="25 October 2024">25 October 2024</span></div>
-          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
-          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
-          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
-                    </div>
-          </div>
-
-          <div className="card1">
-          <div className="card-body">
-          <h2>WHOISDOMAIN</h2>
-          <div class="css-15x27kp"><span class="lbl">Subject</span><span class="val" title="www.netflix.com">www.netflix.com</span></div>
-          <div class="css-15x27kp"><span class="lbl">Issuer</span><span class="val" title="DigiCert Inc">DigiCert Inc</span></div>
-          <div class="css-15x27kp"><span class="lbl">ASN1 Curve</span><span class="val" title="prime256v1">prime256v1</span></div>
-          <div class="css-15x27kp"><span class="lbl">NIST Curve</span><span class="val" title="P-256">P-256</span></div>
-          <div class="css-15x27kp"><span class="lbl">Expires</span><span class="val" title="25 October 2024">25 October 2024</span></div>
-          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
-          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
-          <div class="css-15x27kp"><span class="lbl">Subject</span><span class="val" title="www.netflix.com">www.netflix.com</span></div>
-          <div class="css-15x27kp"><span class="lbl">Issuer</span><span class="val" title="DigiCert Inc">DigiCert Inc</span></div>
-          <div class="css-15x27kp"><span class="lbl">ASN1 Curve</span><span class="val" title="prime256v1">prime256v1</span></div>
-          <div class="css-15x27kp"><span class="lbl">Expires</span><span class="val" title="25 October 2024">25 October 2024</span></div>
-          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
-          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
-          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
-          
-          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
-          </div>
-          </div>
-          </div>
+      <div id="page1">
+        <h1 id="Title">IP VULNERABILTY</h1>
+        <h1>TRACKER</h1>
+        <p>
+          This is used to perform Vulnerability Scanning on the url you enter
+          below.
+        </p>
+        <br />
+        <div className="searchBox">
+          <button className="btnSearch">
+            <i className="fas faSearch"></i>
+          </button>
+          <input
+            type="text"
+            className="inputSearch"
+            id="userInput"
+            ref={inputRef}
+            value={inputValue}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            placeholder="Enter a website's url..."
+          />
         </div>
-    </>
-  )
-}
+      </div>
 
-export default AboutContent
+      <hr />
+      {whoisData && (
+        <CardComponent title="WHOIS Information" data={whoisData} />
+      )}
+    
+    </>
+  );
+ 
+};
+
+WhoIsApiTest();
+
+export default IptrackerContent;

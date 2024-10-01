8c491b42d21de0bee6075c5b5dbc83133ca96ac1
Ameya Taneja <ameyataneja1302@gmail.com>
2024-10-01 12:06:22 +0530

    api response and css fixed

--- src/components/ip-content/ipContent.jsx
+++ src/components/ip-content/ipContent.jsx
@@ -25,20 +25,10 @@ const IpContent = () => {
               }
             );
             console.log("Who is Data");
-          //   const jsonResponse = new DOMParser().parseFromString(response,'text/html')
-          //   const div = jsonResponse.getElementsByTagName('div')[0];
-          //   const paragraphs = div.getElementsByTagName('p');
-          //   const property = paragraphs[0].textContent;
-          //   const value = paragraphs[1].textContent.split(' ')
-          //       .pop().replace('(', '')
-          //       .replace(')', '');
+            // const list = response.data.getElementsByTagName("organisation")[0];
 
-          //   const json = {
-          //         property,
-          //         value,
-          //         };
-          // console.log(JSON.stringify(json, null, 2));
-            console.log(response.data);
+            // console.log(list);
+            console.log(response)
           } catch (error) {
             console.error("Error fetching data:", error);
             setError("Whois lookup failed");
@@ -65,8 +55,8 @@ const IpContent = () => {
 
   return (
     <>
-      <div id="page1">
-        <h1 id="Title">IP VULNERABILITY</h1>
+      <div className="page1">
+        <h1>IP VULNERABILITY</h1>
         <h1>TRACKER</h1>
         <p>
           This is used to perform Vulnerability Scanning on the url you enter
@@ -94,10 +84,23 @@ const IpContent = () => {
       <hr />
 
 
-      <div className="card">
-        <h1>Domain Name: {inputValue}</h1>
-        {/* <h1>Organisation Name: {response.data.Organisation}</h1> */}
-      </div>
+      <div className="card1">
+          <div className="card-body">
+          <h2>WHOISDOMAIN</h2>
+          <div class="css-15x27kp"><span class="lbl">Website</span><span class="val" title="www.netflix.com">{inputValue}</span></div>
+          <div class="css-15x27kp"><span class="lbl">Issuer</span><span class="val" title="DigiCert Inc">DigiCert Inc</span></div>
+          <div class="css-15x27kp"><span class="lbl">ASN1 Curve</span><span class="val" title="prime256v1">prime256v1</span></div>
+          
+          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
+          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
+          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
+          <div class="css-15x27kp"><span class="lbl">NIST Curve</span><span class="val" title="P-256">P-256</span></div>
+          <div class="css-15x27kp"><span class="lbl">Expires</span><span class="val" title="25 October 2024">25 October 2024</span></div>
+          <div class="css-15x27kp"><span class="lbl">Renewed</span><span class="val" title="25 October 2023">25 October 2023</span></div>
+          <div class="css-15x27kp"><span class="lbl">Serial Num</span><span class="val" title="0BE2A20B61333D0458E2044D79A9185F">0BE2A20B61333D0458E2044D79A9185F</span></div>
+          <div class="css-15x27kp"><span class="lbl">Fingerprint</span><span class="val" title="6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D">6D:5C:DB:96:AE:DA:1C:F7:37:30:A4:4A:F8:F4:84:12:BD:74:91:7D</span></div>
+                    </div>
+          </div>
     </>
   );
 };

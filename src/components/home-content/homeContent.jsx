64c86107511018340d6d68de48e925b136d54878
Akshat-NegI27 <141574274+Akshat-NegI27@users.noreply.github.com>
2024-11-16 22:01:44 +0530

    New Update for Optimization

--- src/components/home-content/homeContent.jsx
+++ src/components/home-content/homeContent.jsx
@@ -1,17 +1,33 @@
 import React,{ useEffect } from "react";
 import "./homeContent.css";
 
+import arrow from '../../img/arrow_upward.svg';
+
 import img1 from '../../img/rd.svg';
 import img2 from '../../img/webby.svg';
 import img3 from '../../img/pmi.svg';
 import img4 from '../../img/wf.svg';
 import img5 from '../../img/adweek.svg';
 import img6 from '../../img/forbes.svg';
-import Video from "../../img/video.mp4";
+
+import imgpg2 from '../../img/pg21.webp';
+import Video from "../../img/vip.mp4";
 import img1pg4 from '../../img/grid1.jpg';
-import img2pg4 from '../../img/grid2.jpg';
-import arrow from '../../img/arrow_upward.svg';
-import imgpg21 from '../../img/pg21.webp';
+import img2pg5 from '../../img/grid2.jpg';
+
+// Slider Images
+import slide1 from '../../img/slide1.webp';
+import slide2 from '../../img/slide2.webp';
+
+import slide21 from '../../img/slide21.webp';
+import slide22 from '../../img/slide22.jpg';
+import slide23 from '../../img/slide22.jpg';
+import slide24 from '../../img/slide22.jpg';
+
+import slide31 from '../../img/slide22.jpg';
+import slide32 from '../../img/slide32.png';
+import slide33 from '../../img/slide33.jpg';
+
 
 import Aos from "aos";
 import 'aos/dist/aos.css'
@@ -148,14 +164,14 @@ const HomeContent = () => {
             </div>
 
             <div className="move">
-              <img src={img1} alt="" />
-              <img src={img2} alt="" />
-              <img src={img3} alt="" />
-              <img src={img4} alt="" />
-              <img src={img5} alt="" />
-              <img src={img6} alt="" />
-              <img src={img3} alt="" />
-              <img src={img4} alt="" />
+              <img loading="lazy" src={img1} alt="" />
+              <img loading="lazy" src={img2} alt="" />
+              <img loading="lazy" src={img3} alt="" />
+              <img loading="lazy" src={img4} alt="" />
+              <img loading="lazy" src={img5} alt="" />
+              <img loading="lazy" src={img6} alt="" />
+              <img loading="lazy" src={img3} alt="" />
+              <img loading="lazy" src={img4} alt="" />
             </div>
             <div id="blur-right"></div>
           </div>
@@ -174,9 +190,8 @@ const HomeContent = () => {
               <h2 data-scroll data-scroll-speed="0.1" > WEB Vulnerabily Detection</h2>
               <h3 data-scroll data-scroll-speed="0.1" >Now Hands ON</h3>  
               </div>
-              <div data-scroll data-scroll-speed="0.1" className="image-container">
-              {/* <img className="hover-image" src={img2pg4} alt="" /> */}
-              <img className="hover-image" src={imgpg21} alt="" />
+              <div data-scroll data-scroll-speed="0.1" className="image-containerx">
+              <img loading="lazy" className="hover-image" src={imgpg2} alt="" />
               </div>
 
 
@@ -207,60 +222,12 @@ const HomeContent = () => {
               </div>
           </div>
         </div>
-        
-
-        {/* <div className="page4">
-          <div className="border">
-            <h1>NEW CONTENT</h1>
-            <div className="imagesbox">
-          <img  className="img41" src={img1pg4} alt="" />
-          <img  className="img42" src={img2pg4} alt="" />
-          </div>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae error expedita aspernatur illum odit accusantium repellat itaque iusto natus commodi, obcaecati atque, non aliquam molestias earum, iure placeat? Modi, optio.</p>
-          </div>
-        </div> */}
-
-{/* 
-        <div id="page4">
-        <img className="imgpg4bg1" src={imgpg4bg1} alt="" />
-        <img className="imgpg4bg2" src={imgpg4bg2} alt="" />
-        <div id= "images">
-            <video className="img2" autoPlay loop muted src={Videocsf} type="video/mp4"/>
-            <img  className="img1"  src={imgpg4}  alt="" />
-            <video className="img3" autoPlay loop muted src={Video} type="video/mp4"/>
-
-            <img  className="img41" src={img441} alt="" />
-            <img  className="img42" src={img442} alt="" />
-            <img  className="img43" src={img443} alt="" />
-            <video className="img44" autoPlay loop muted src={Videovip} type="video/mp4"/>
-
-            <img  className="img45" src={img445} alt="" />
-          </div>
-          
-        <div  id="captions">
-            <div id="card1">
-                <h1>Resource-Intensive Processing</h1>
-                <p>The computational demands of real-time IP traffic analysis, especially in large-scale networks, can be significant. This may require substantial investment in hardware and computational resources, potentially limiting its accessibility to organizations with smaller IT budgets.</p>
-            </div>
-
-            <div  id="card2">
-                <h2>Corporate Network Security</h2>
-                <p>In the corporate world, safeguarding sensitive data and ensuring the smooth operation of business processes are paramount. The system can be deployed across enterprise networks to continuously monitor for anomalies that could indicate potential security breaches.</p>
-            </div>
-
-            <div  id="card3">
-                <h2>At Visual Identity,<br></br><span>we're not just another</span> Program</h2>
-            </div>
-
-            </div>
-        </div> */}
-
 
         {/*-------------Page-4------------- */}
         <div className="page4">
           <div className="box">
             <div className="image-container image">
-              <img className="hover-image" src={img1pg4} alt="" />
+              <img loading="lazy" className="hover-image" src={img1pg4} alt="" />
             </div>
 
             <div className="text-content">
@@ -274,7 +241,7 @@ const HomeContent = () => {
         <div className="page5">
           <div className="box">
             <div className="image-container image">
-              <img className="hover-image" src={img2pg4} alt="" />
+              <img loading="lazy" className="hover-image" src={img2pg5} alt="" />
             </div>
             <div className="text-content">
               <h1>NEW CONTENT</h1>
@@ -305,27 +272,24 @@ const HomeContent = () => {
       
         <SwiperSlide>
           <div className="slidess">
-          <img src="https://images.unsplash.com/photo-1723643135768-8a57eb0959d4?q=80&w=1601&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img src="https://images.unsplash.com/photo-1719094251938-e596af9261de?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
-          " alt="" />
+          <img src={slide1} alt="" />
+          <img src={slide2} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slides">
-          <img src="https://images.unsplash.com/photo-1723643135768-8a57eb0959d4?q=80&w=1601&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img src="https://images.unsplash.com/photo-1723643136002-d49a1d7309d1?q=80&w=1414&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img src="https://images.unsplash.com/photo-1632283409723-4cd4560e0857?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img src="https://images.unsplash.com/photo-1719094251938-e596af9261de?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
-          " alt="" />
+          <img src={slide21} alt="" />
+          <img src={slide22} alt="" />
+          <img src={slide23} alt="" />
+          <img src={slide24} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="slides">
-          <img src="https://images.unsplash.com/photo-1723643135768-8a57eb0959d4?q=80&w=1601&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img src="https://images.unsplash.com/photo-1723643136002-d49a1d7309d1?q=80&w=1414&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img src="https://images.unsplash.com/photo-1632283409723-4cd4560e0857?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img src="https://images.unsplash.com/photo-1719094251938-e596af9261de?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
-          " alt="" />
+          <div className="slidesx">
+          <img src={slide31} alt="" />
+          <img src={slide32} alt="" />
+          <img src={slide33} alt="" />
+
           </div>
         </SwiperSlide>
 
--- src/components/login-content/login.jsx
+++ src/components/login-content/login.jsx
@@ -1,53 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import './login.css';
-
+import { Link } from 'react-router-dom';
 const Login = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isSignup, setIsSignup] = useState(false);
 
-  const handleToggle = () => {
-    setIsLogin(!isLogin);
+  const toggleForm = () => {
+    setIsSignup(!isSignup);
   };
 
   return (
-
-    <div className="lpage">
-
-    <div className="auth-container">
-      <div className={`form-container ${isLogin ? "login" : "signup"}`}>
-        {isLogin ? <LoginForm /> : <SignupForm />}
+    <div className='login-page'>
+    <div className={`container ${isSignup ? 'right-panel-active' : ''}`} id="container">
+        {/* Sign Up Form */}
+      <div className="form-containerr sign-up-container">
+        <form action="#">
+        <div className="xxx">
+          <h1>Create Account</h1>
+          <div className="social-container">
+            <Link to="#" className="social"><i className="fab fa-facebook-f"></i></Link>
+            <Link to="#" className="social"><i className="fab fa-google-plus-g"></i></Link>
+            <Link to="#" className="social"><i className="fab fa-linkedin-in"></i></Link>
+          </div>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+          </div>
+        </form>
       </div>
-      <div className="toggle-container">
-        <p>{isLogin ? "Don't have an account?" : "Already have an account?"}</p>
-        <button onClick={handleToggle}>
-          {isLogin ? "Sign Up" : "Log In"}
-        </button>
+
+      {/* Sign In Form */}
+      <div className="form-containerr sign-in-container">
+        <form action="#">
+            <div className="xxx">
+          <h1>Welcome Back!</h1>
+          <div className="social-container">
+          <Link id='icon' to="#" className="social"><i className="fab fa-facebook-f"></i></Link>
+          <Link id='icon' to="#" className="social"><i className="fab fa-google-plus-g"></i></Link>
+          <Link id='icon' to="#" className="social"><i className="fab fa-linkedin-in"></i></Link>
+          </div>
+          <h3>Email</h3>
+          <input type="email" placeholder="Email" />
+          <h3>Password</h3>
+          <input type="password" placeholder="Passwordd" />
+          <br />
+          <Link id="forgot" to="#">Forgot your password?  </Link>
+          <br />
+          <button>Sign In</button>
+          </div>
+        </form>
+      </div>
+
+        {/* Overlay */}
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us please login with your personal info</p>
+            <button className="ghost" id="signIn" onClick={toggleForm}>Sign In</button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button className="ghost" id="signUp" onClick={toggleForm}>Sign Up</button>
+          </div>
+        </div>
       </div>
     </div>
     </div>
-
-  );
-};
-
-const LoginForm = () => {
-  return (
-    <form className="form">
-      <h2>Login</h2>
-      <input type="email" placeholder="Email" required />
-      <input type="password" placeholder="Password" required />
-      <button type="submit">Log In</button>
-    </form>
-  );
-};
-
-const SignupForm = () => {
-  return (
-    <form className="form">
-      <h2>Sign Up</h2>
-      <input type="text" placeholder="Username" required />
-      <input type="email" placeholder="Email" required />
-      <input type="password" placeholder="Password" required />
-      <button type="submit">Sign Up</button>
-    </form>
   );
 };
 
